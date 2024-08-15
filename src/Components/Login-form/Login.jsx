cc6f912fba0f1356b2f0c9d34767e2b456ab7950
anishsaini <anishoficial@gmail.com>
2024-08-15 12:25:04 +0530

    account signup or login form is completed

--- src/Components/Login-form/Login.jsx
+++ src/Components/Login-form/Login.jsx
@@ -1,5 +1,77 @@
+import "../../App.css";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
+import {
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Login Successful");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleForgotPassword = async () => {
+    if (!email) {
+      alert("Please enter your email address first.");
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset email sent. Please check your inbox.");
+    } catch (error) {
+      alert("Error sending password reset email: " + error.message);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+        <br />
+        <p>
+          Don't have an account? <Link to="/signup">Signup</Link>
+        </p>
+        <p>
+          <button type="btn" onClick={handleForgotPassword}>
+            Forgot Password?
+          </button>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Login-form/Signup.jsx
+++ src/Components/Login-form/Signup.jsx
@@ -1,21 +1,44 @@
 import "../../App.css";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { useState } from "react";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Signup Successful");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div className="signup-container">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <h2>Signup</h2>
-        <label htmlFor="Email">
+        <label htmlFor="email">
           Email:
-          <input type="text" />
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
         </label>
-        <label htmlFor="Password">
+        <label htmlFor="password">
           Password:
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button type="submit">Signup</button>
+        <br />
         <p>
-          Already Registered?<a>Login</a>
+          Already Registered? <Link to="/login">Login</Link>
         </p>
       </form>
     </div>
